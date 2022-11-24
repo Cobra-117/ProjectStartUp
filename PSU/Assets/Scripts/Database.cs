4f0cdad71ce851e14ba3616375c600919c7d4ae0
Timothé Lacheroy <tLacheroy@localhost.localdomain>
2022-11-24 11:17:55 +0100

    move recipe database to resources folder

--- PSU/Assets/Scripts/Database.cs
+++ PSU/Assets/Scripts/Database.cs
@@ -24,32 +24,37 @@ public class Database : MonoBehaviour
         test.isVegan = true;
         test.isVegetarian = true;
         test.name = "test";
+        test.tags = new string [] {"test1", "test2"};
         string json = JsonUtility.ToJson(test);
         Debug.Log(json);
-        SaveData(json, "save");
-        Recipe testLoad = LoadRecipe("save");
+        //SaveRecipe(json, "save");
+        Recipe testLoad = LoadRecipe("1");
         Debug.Log("name : " + testLoad.name);
+        Debug.Log("name : " + testLoad.tags[0]);
+        /*TextAsset textAsset =  Resources.Load<TextAsset>("1");
+        Recipe recipe = new Recipe();
+        recipe = JsonUtility.FromJson<Recipe>(textAsset.text);*/
+        Debug.Log("recipe name : " + testLoad.name);
+
     }
 
-    public void SaveData(string data, string filename)
+    /*Do not use for now*/
+    public void SaveRecipe(string data, string filename)
     {
-        string dir = Application.persistentDataPath + "/" + filename;
+        string dir = Application.persistentDataPath + "/recipes/recipes/" + filename;
         File.WriteAllText(dir, data);
     }
 
+    /*Load a recipe from a json file 
+    filename : the name of the database file, without .json*/
     public Recipe LoadRecipe(string filename)
     {
-        string path = Application.persistentDataPath + "/" + filename;
+        string path = Application.dataPath + "database/recipes/recipes/" + filename;
         Recipe recipe = new Recipe();
-
-        if (File.Exists(path)) {
-            string json = File.ReadAllText(path);
-            recipe = JsonUtility.FromJson<Recipe>(json);
-        }
-        else 
-        {
-            Debug.LogError("This recipe file do not exist");
-        }
+        string json = Resources.Load<TextAsset>(
+            "Databases/Recipes/Recipes/"  + filename).text;
+        recipe = JsonUtility.FromJson<Recipe>(json);
+      
         return recipe;
     }
 }
