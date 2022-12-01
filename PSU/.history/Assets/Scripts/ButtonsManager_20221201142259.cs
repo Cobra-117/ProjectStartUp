e6c1ac7906dfb454741ee0b1f7f4350573c69f98
VnssJT <64807616+VnssJT@users.noreply.github.com>
2022-12-01 14:51:45 +0100

    buttons v1

---
+++ PSU/.history/Assets/Scripts/ButtonsManager_20221201142259.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonsManager : MonoBehaviour
+{
+    [SerializeField] private GameObject userSettingsOverlay;
+    [SerializeField] private GameObject preferencesOverlay;
+    [SerializeField] private GameObject joinRoomOverlay;
+    [SerializeField] private GameObject createRoomOverlay;
+
++
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PSU/.history/Assets/Scripts/MainButtons_20221201142722.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtons : MonoBehaviour
+{
+    [SerializeField] private GameObject selectedDot;
+
+    public void isSelected(boolean isSelected)
+    
+}
